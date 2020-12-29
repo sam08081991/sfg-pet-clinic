66b49af94f07d5d9bf4c0829e1c4376137bb32e8
Thuong Nguyen Khac Sam <thuong.nguyenkhacsam@axonactive.com>
2020-12-29 18:11:28 +0700

    Implement POJO (in progress)

---
+++ src/main/java/com/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,28 @@
+package com.springframework.sfgpetclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class Person extends BaseEntity {
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+}
