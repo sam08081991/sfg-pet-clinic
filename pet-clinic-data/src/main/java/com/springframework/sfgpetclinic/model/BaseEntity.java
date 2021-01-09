fc75e5ab4ff536624d0cdf95fb9c4b165845b6af
ksamtn <thuong.nguyenkhacsam@axonactive.com>
2021-01-09 22:37:55 +0700

    Create PetType Map Service Impl. Closes #29

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/BaseEntity.java
@@ -10,14 +10,20 @@ import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public boolean isNew() {
         return this.id == null;
