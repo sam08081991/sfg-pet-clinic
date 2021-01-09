79d05a54255c18d45d4c6bbd11d00d4fb2c9dc3b
ksamtn <thuong.nguyenkhacsam@axonactive.com>
2021-01-09 22:21:21 +0700

    Create Vet Speciality Entity, Associate To Vet. Closes #27

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,17 @@
 package com.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
-    public Vet() {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 
     public Vet(Long id, String firstName, String lastName) {
