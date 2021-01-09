c575bd7a79553669dc641873461ca1dc0a6cfc0b
ksamtn <thuong.nguyenkhacsam@axonactive.com>
2021-01-09 22:14:45 +0700

    Create Pet Type, Pet, and Visit Entities. Closes #26

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -2,8 +2,19 @@ package com.springframework.sfgpetclinic.model;
 
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @NoArgsConstructor
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
     public Owner(Long id, String firstName, String lastName) {
         super(id, firstName, lastName);
